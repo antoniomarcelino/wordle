2e1645e37bb8cdeebd59e4aeddd1e0e941d73ec7
antonio <aanttmarck@gmail.com>
2022-10-13 23:41:50 -0300

    serverless

--- src/App.js
+++ src/App.js
@@ -7,13 +7,26 @@ function App() {
   const[solution, setSolution] = useState(null)
 
   useEffect(()=> {
-    fetch('http://localhost:3001/solutions')
-    .then(res => res.json())
-    .then(json => {
-      const randomSolution = json[Math.floor(Math.random() * json.length)]
+      const solutions = [
+        {"id": 1, "word": "ninja"},
+        {"id": 2, "word": "sagaz"},
+        {"id": 3, "word": "cores"},
+        {"id": 4, "word": "algoz"},
+        {"id": 5, "word": "plena"},
+        {"id": 6, "word": "suave"},
+        {"id": 7, "word": "honra"},
+        {"id": 8, "word": "teste"},
+        {"id": 9, "word": "justo"},
+        {"id": 10, "word": "haver"},
+        {"id": 11, "word": "sonho"},
+        {"id": 12, "word": "amigo"},
+        {"id": 13, "word": "etnia"},
+        {"id": 14, "word": "cones"},
+        {"id": 15, "word": "audaz"}
+      ]
+      const randomSolution = solutions[Math.floor(Math.random() * solutions.length)]
       setSolution(randomSolution.word)
-    })
-  }, [setSolution])
+    }, [setSolution])
 
   return (
     <div className="App">
@@ -23,44 +36,13 @@ function App() {
   );
 }
 
+/*useEffect(()=> {
+    fetch('http://localhost:3001/solutions')
+    .then(res => res.json())
+    .then(json => {
+      const randomSolution = json[Math.floor(Math.random() * json.length)]
+      setSolution(randomSolution.word)
+    })
+  }, [setSolution])*/
+
 export default App
-
-/* 
-
-data we need to track:
-  -- solution
-    -- 5 letter string, e.g. 'drain'
-  -- past guesses
-    -- an array of past guesses
-    -- each past guess is an array of letter objects [{}, {}, {}, {}, {}]
-    -- each object represents a letter in the guess word {letter: 'a', color: 'yellow'}
-  -- current guess
-    -- string 'hello'
-  -- keypad letters
-    -- array of letter objects [{key: 'a', color: 'green'}, {}, {} ...]
-  -- number of turns
-    -- an integer 0 - 6
-
-game process:
-  -- entering words:
-    -- user enters a letter & a square is filled with that letter
-    -- when a user hits delete it deletes the previous letter
-    -- when a user hits enter it submits the word
-      -- if all squares are not filled with letters then the word is not submitted
-      -- if that word has already been used in a prev guess then the word is not submitted
-  -- checking submitted words:
-    -- each letter is checked to see if it matches to the solution
-    -- each letter is assigned a color based on it's inclusion in the solution
-      -- exact matches (correct position in the solution) are green
-      -- partial matches (in the solution but not the correct position) are yellow
-      -- none-matches (not in the solution at all) are grey
-    -- the guess is added the grid with the correct colors
-    -- the current guess moves to the next row
-    -- the keypad letters are updated (colors)
-  -- ending the game:
-    -- when the guessed word fully matches the solution
-      -- modal to say 'well done'
-    -- when the user runs out of guesses
-      -- modal to say 'unlucky'
-
-*/
